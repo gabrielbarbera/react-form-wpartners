ca9371b137878f1f54dcb5cbe8da96923dd3fbab
Gabriel Barbera <gabrielbarbera@gmail.com>
2019-11-07 05:39:02 +0100

    Updates

---
+++ precache-manifest.43decd1648fbc6b37f7428d49fdf532e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b205c4f304189e5da3e20b10c5d8cbb3",
+    "url": "/react-form-wpartners/index.html"
+  },
+  {
+    "revision": "3c43ab97740186fc1929",
+    "url": "/react-form-wpartners/static/css/main.3983206c.chunk.css"
+  },
+  {
+    "revision": "43e976226d62c4a647f9",
+    "url": "/react-form-wpartners/static/js/2.45fd083a.chunk.js"
+  },
+  {
+    "revision": "3c43ab97740186fc1929",
+    "url": "/react-form-wpartners/static/js/main.cfa65c60.chunk.js"
+  },
+  {
+    "revision": "d1073669f4ea145fd518",
+    "url": "/react-form-wpartners/static/js/runtime-main.716d53a1.js"
+  }
+]);
